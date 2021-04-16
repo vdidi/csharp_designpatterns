64532b21ec0d8d028478093a7f64ba07753af5ae
vdidi <vitor_hugo_di@hotmail.com>
2021-04-16 08:22:59 -0300

    feat: add files

---
+++ 02/demos/Exercise Files/M01_Starter/Builder Pattern/InventoryReport.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Builder_Pattern
+{
+    public class FurnitureItem
+    {
+        public string Name;
+        public double Price;
+        public double Height;
+        public double Width;
+        public double Weight;
+
+        public FurnitureItem(string productName, double price, double height, double width, double weight)
+        {
+            this.Name = productName;
+            this.Price = price;
+            this.Height = height;
+            this.Width = width;
+            this.Weight = weight;
+        }
+    }
+
+    public class InventoryReport
+    {
+        public string TitleSection;
+        public string DimensionsSection;
+        public string LogisticsSection;
+
+        public string Debug()
+        {
+            return new StringBuilder()
+                .AppendLine(TitleSection)
+                .AppendLine(DimensionsSection)
+                .AppendLine(LogisticsSection)
+                .ToString();
+        }
+    }
+}
---
+++ 02/demos/Exercise Files/M01_Starter/Builder Pattern/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Builder_Pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var items = new List<FurnitureItem>
+            {
+                new FurnitureItem("Sectional Couch", 55.5, 22.4, 78.6, 35.0),
+                new FurnitureItem("Nightstand", 25.0, 12.4, 20.0, 10.0),
+                new FurnitureItem("Dining Table", 105.0, 35.4, 100.6, 55.5),
+            };
+        }
+    }
+}
