92ee37e24495833624049816f5be5612b47e8fa2
vdidi <vitor_hugo_di@hotmail.com>
2021-04-19 08:19:56 -0300

    refactor: inject ShippingProvider to ShoppingCart

--- 02/demos/Demo 1 - Factory Pattern First Look/Start_Here/Business/ShoppingCart.cs
+++ 02/demos/Demo 1 - Factory Pattern First Look/Start_Here/Business/ShoppingCart.cs
@@ -7,15 +7,17 @@ namespace Factory_Pattern_First_Look.Business
     public class ShoppingCart
     {
         private readonly Order order;
+        private readonly ShippingProviderFactory shippingProviderFactory;
 
-        public ShoppingCart(Order order)
+        public ShoppingCart(Order order, ShippingProviderFactory shippingProviderFactory)
         {
             this.order = order;
+            this.shippingProviderFactory = shippingProviderFactory;
         }
 
         public string Finalize()    
         {
-            var shippingProvider = ShippingProviderFactory.CreateShippingProvider(order.Sender.Country);
+            var shippingProvider = shippingProviderFactory.GetShippingProvider(order.Sender.Country);
 
             order.ShippingStatus = ShippingStatus.ReadyForShippment;
 
